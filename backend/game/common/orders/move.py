59b08b5a8842bd1bee6f51bcce108bb0a0010073
maximalekseenko <max.alekseenko@gmail.com>
2022-10-16 00:21:35 +0300

    move formula fix

--- backend/game/common/orders/move.py
+++ backend/game/common/orders/move.py
@@ -6,25 +6,29 @@ class Move(Order):
     NAME = "MOVE"
     TYPE = "MOVEMENT"
 
+
     def Fix_Target(self, target:tuple[int, int]=None) -> any:
         if target == None: return
 
-        delta_X = self.vessel.position[0] - target[0]
-        delta_Y = self.vessel.position[1] - target[1]
+        delta_X = target[0] - self.vessel.position[0]
+        delta_Y = target[1] - self.vessel.position[1]
 
-        # direction fix
-        # print(degrees(atan2(delta_X, delta_Y)))
+        # direction check
+        if (self.vessel.rotation - degrees(atan2(delta_Y, delta_X))) % 360 > 90: return None
 
-        # position fix
+        # get distance
         sinner = sin(self.vessel.rad_rotation)
-        cosinner = cos(self.vessel.rad_rotation)
-        a = (sinner * -delta_X + cosinner * -delta_Y)
+        cosinner = -cos(self.vessel.rad_rotation)
+        distance = (cosinner * delta_X + sinner * delta_Y)
 
-        # round fix
+        # fix distance
+        # TODO
+        # if distance > self.vessel.move
+
         target = (
-            self.game.Round(self.vessel.position[0] + a * sinner),
-            self.game.Round(self.vessel.position[1] + a * cosinner)
-        )
+            self.game.Round(self.vessel.position[0] + distance * cosinner),
+            self.game.Round(self.vessel.position[1] + distance * sinner))
+
 
         return target
 
@@ -41,14 +45,26 @@ class Move(Order):
     def _Step(self, target:tuple[int, int]=None):
 
         # get new position
-        new_x = self.vessel.position[0] - sin(self.vessel.rad_rotation)
-        new_y = self.vessel.position[1] + cos(self.vessel.rad_rotation)
+        new_x = self.vessel.position[0] + cos(self.vessel.rad_rotation)
+        new_y = self.vessel.position[1] - sin(self.vessel.rad_rotation)
+
+        delta_X = target[0] - new_x
+        delta_Y = target[1] - new_y
 
         # if finished movement
-        if abs(new_x - target[0]) < 1 or abs(new_y - target[1]) < 1:
+        # print(self.vessel.rotation, cos(self.vessel.rad_rotation), sin(self.vessel.rad_rotation))
+        # print(delta_X, delta_Y, degrees(atan2(delta_Y, delta_X)))
+        print("A",new_x, new_y)
+        if (self.vessel.rotation - degrees(atan2(delta_Y, delta_X))) % 360 > 90:
             self.vessel.position = target
+            return
         else: 
             self.vessel.position = (new_x, new_y)
+            self._Test()
 
         self._Step(target)
+
+    
+    def _Test():
+        pass
 
--- test.py
+++ test.py
@@ -6,20 +6,20 @@ game = Game()
 
 # player 1
 game.Join(1)
-vessel1p1 = game.Add_Vessel(LunarClassCruiser, 0, (0, 20 ), -135)
-vessel2p1 = game.Add_Vessel(LunarClassCruiser, 0, (0, 30 ), -90)
-vessel3p1 = game.Add_Vessel(LunarClassCruiser, 0, (0, 40 ), -90)
+vessel1p1 = game.Add_Vessel(LunarClassCruiser, 0, (10, 20 ), 0)
+vessel2p1 = game.Add_Vessel(LunarClassCruiser, 0, (10, 30 ), 0)
+vessel3p1 = game.Add_Vessel(LunarClassCruiser, 0, (10, 40 ), 0)
 
 # player 2
 game.Join(2)
-vessel1p2 = game.Add_Vessel(LunarClassCruiser, 1, (90, 20), 90)
-vessel2p2 = game.Add_Vessel(LunarClassCruiser, 1, (90, 30), 90)
-vessel3p2 = game.Add_Vessel(LunarClassCruiser, 1, (90, 40), 45)
+vessel1p2 = game.Add_Vessel(LunarClassCruiser, 1, (80, 20), 180)
+vessel2p2 = game.Add_Vessel(LunarClassCruiser, 1, (80, 30), 180)
+vessel3p2 = game.Add_Vessel(LunarClassCruiser, 1, (80, 40), 180)
 
 # start
 game.Start()
 
 
 print(vessel1p1.position)
-game.Handle_Order(1, vessel1p1.Get_Order_By_Id(0).Get(target=(10, 10)))
+game.Handle_Order(1, vessel1p1.Get_Order_By_Id(0).Get(target=(20, 20)))
 print(vessel1p1.position)
