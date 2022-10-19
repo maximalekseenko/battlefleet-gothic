72c1b1ee6f25f8159523bdd9741d5b9afd6de738
maximalekseenko <max.alekseenko@gmail.com>
2022-10-19 08:58:51 +0300

    black color added

--- frontend/theatre/color.py
+++ frontend/theatre/color.py
@@ -48,5 +48,11 @@ class COLOR:
     CYANd1      = "#008080"
     CYANd2      = "#006060"
 
+    BLACKl2      = "#c0c0c0"
+    BLACKl1      = "#a0a0a0"
+    BLACK        = "#808080"
+    BLACKd1      = "#606060"
+    BLACKd2      = "#202020"
+
     def __getitem__(self, key):
         return getattr(self, key, "#000000")
